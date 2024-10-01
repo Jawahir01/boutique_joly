a3aff3e81c3dd0d13247d2a9ef3ae1732c2a5b27
Jawahir Abdul-Bari <714591@student.cityofbristol.ac.uk>
2024-10-01 11:57:44 +0000

    Added shopping bag app, urls and template

--- boutique_joly/settings.py
+++ boutique_joly/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',    # Optional -- requires install using `django-allauth[socialaccount]`.
     'home',
     'products',
+    'bag',
 ]
 
 MIDDLEWARE = [
