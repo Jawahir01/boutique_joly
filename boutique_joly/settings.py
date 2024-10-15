df38f310331a3942522dd8c81c43fd3f207f5a1f
Jawahir Abdul-Bari <714591@student.cityofbristol.ac.uk>
2024-10-15 12:43:10 +0000

    Added toasts

--- boutique_joly/settings.py
+++ boutique_joly/settings.py
@@ -84,6 +84,8 @@ TEMPLATES = [
     },
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AAUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
